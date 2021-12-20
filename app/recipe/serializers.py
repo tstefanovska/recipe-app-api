ff8e2447a2f4eb9077cf8ea16ee991330cb5126f
WF | Teodora Stefanovska <teodora.stefanovska@webfactory.mk>
2021-12-20 13:57:54 +0100

    Added custom detail view

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -37,3 +37,9 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'time_minutes', 'price',
                   'ingredients', 'tags')
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
